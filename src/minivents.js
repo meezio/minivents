19191a6833d949182adb1d4a8c874aec662852d7
Anthony <anthony@minotti.cool>
2017-10-05 18:00:20 +0200

    Add JSDoc

--- src/minivents.js
+++ src/minivents.js
@@ -2,37 +2,47 @@ function Events(target){
   var events = {}, empty = [];
   target = target || this
   /**
-   *  On: listen to events
+   *  Listen to events.
+   * @param {string} event - The event's name to listen to.
+   * @param {function} callback - The function to execute when the event is triggered.
+   * @param {Object} ctx - The value of this provided for the call to the callback function.
+   * @param {boolean} [once=false] - The callback will be executed at most once.
    */
-  target.on = function(type, func, ctx, once){
+  target.on = function(event, callback, ctx, once){
     once = !!once;
-    (events[type] = events[type] || []).push([func, ctx, once])
+    (events[event] = events[event] || []).push([callback, ctx, once])
     return target
   }
   /**
-   *  One: listen to events once
+   *  Listen to events once.
+   * @param {string} event - The event's name to listen to.
+   * @param {function} callback - The function to execute when the event is triggered.
+   * @param {Object} ctx - The value of this provided for the call to the callback function.
    */
-  target.one = function(type, func, ctx){
-    return target.on(type, func, ctx, true)
+  target.one = function(event, callback, ctx){
+    return target.on(event, callback, ctx, true)
   }
   /**
-   *  Off: stop listening to event / specific callback
+   *  Stop listening to event.
+   * @param {string} event - The event's name to stop listen to.
+   * @param {function} [callback] - If provided, stop listening only for this callback.
    */
-  target.off = function(type, func){
-    type || (events = {})
-    var list = events[type] || empty, i = list.length = func ? list.length : 0;
+  target.off = function(event, callback){
+    event || (events = {})
+    var list = events[event] || empty, i = list.length = callback ? list.length : 0;
     while(i--)
-        func == list[i][0] && list.splice(i,1)
+        callback == list[i][0] && list.splice(i,1)
     return target
   }
   /** 
-   * Emit: send event, callbacks will be triggered
+   * Send event, callbacks will be triggered.
+   * @param {string} event - The name of the event to trigger.
    */
-  target.trigger = function(type){
-    var e = events[type] || empty, list = e.length > 0 ? e.slice(0, e.length) : e, i=0, j;
+  target.trigger = function(event){
+    var e = events[event] || empty, list = e.length > 0 ? e.slice(0, e.length) : e, i=0, j;
     while(j=list[i++]){
         j[0].apply(j[1], empty.slice.call(arguments, 1));
-        if(j[2]) target.off(type, j[0]);
+        if(j[2]) target.off(event, j[0]);
     }
     return target
   };
